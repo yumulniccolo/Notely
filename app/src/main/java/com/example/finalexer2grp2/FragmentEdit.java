baac8b05d5bc4d40bc2c2e9f2bda339ef2404e5a
Niccolo Yumul <yumul_niccolofranco@plpasig.edu.ph>
2026-04-19 21:55:36 +0800

    Minimum Fragments and Navigation done
    All the proper fragments and navigation is complete

---
+++ app/src/main/java/com/example/finalexer2grp2/FragmentEdit.java
@@ -0,0 +1,36 @@
+package com.example.finalexer2grp2;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentEdit extends Fragment {
+
+    private FragmentEditViewModel mViewModel;
+
+    public static FragmentEdit newInstance() {
+        return new FragmentEdit();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edit, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FragmentEditViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/finalexer2grp2/FragmentEditViewModel.java
@@ -0,0 +1,7 @@
+package com.example.finalexer2grp2;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragmentEditViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
